beae4acde60b087aa38b12b3f6d7836cf58030c0
goranc <cabarkapa.goran@gmail.com>
2021-05-18 18:32:04 +0200

    Working on mentors/pannelist section, fixed some overall responsiveness issues due to images not scaling appropriately, done for today

--- src/components/mentor/Mentor.js
+++ src/components/mentor/Mentor.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import mentor1 from 'assets/mentor1.png';
+import mentor2 from 'assets/mentor2.png';
+import mentor3 from 'assets/mentor3.png';
+import mentor4 from 'assets/mentor4.png';
+import mentors from '../../data';
 import './mentor.scss';
+
 const Mentor = () => {
-  return <div className="mentor">Mentor section</div>;
+  console.log(mentors);
+  return (
+    <div className="mentor">
+      <div className="wrapper">
+        <div className="topContent">
+          <div className="leftTopContent">
+            <div className="imgWrapper">
+              <img src={mentor1} alt="illustration" />
+            </div>
+            <div className="imgWrapper">
+              <img src={mentor2} alt="illustration" />
+            </div>
+            <div className="imgWrapper">
+              <img src={mentor3} alt="illustration" />
+            </div>
+            <div className="imgWrapper">
+              <img src={mentor4} alt="illustration" />
+            </div>
+          </div>
+          <div className="rightTopContent">
+            <h1 className="title">
+              Ko su mentori i učesnici frilenserskog panela?
+            </h1>
+          </div>
+        </div>
+        <div className="bottomContent">
+          {mentors.map((mentor) => (
+            <div key={mentor.id} className="mentor">
+              <div className="imgWrapper">
+                <img src={mentor.avatar} alt="avatar" className="avatar" />
+              </div>
+              <div className="mentorInfo">
+                <span className="name">{mentor.name}</span>
+                <span className="employment">{mentor.employment}</span>
+                <span className="role">{mentor.role}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Mentor;
---
+++ src/data.js
@@ -0,0 +1,116 @@
+import avatar1 from 'assets/1.png';
+import avatar2 from 'assets/2.png';
+import avatar3 from 'assets/3.png';
+import avatar4 from 'assets/4.png';
+import avatar5 from 'assets/5.png';
+import avatar6 from 'assets/6.png';
+import avatar7 from 'assets/7.png';
+import avatar8 from 'assets/8.png';
+import avatar9 from 'assets/9.png';
+import avatar10 from 'assets/10.png';
+import avatar11 from 'assets/11.png';
+import avatar12 from 'assets/12.png';
+import avatar13 from 'assets/13.png';
+import avatar14 from 'assets/14.png';
+const mentors = [
+  {
+    id: '1',
+    name: 'Jovan Cicmil',
+    employment: 'Software Development',
+    role: 'Mentor',
+    avatar: avatar1,
+  },
+  {
+    id: '2',
+    name: 'Dragan Čabarkapa',
+    employment: 'Software Development',
+    role: 'Mentor, Panelista, Moderator',
+    avatar: avatar2,
+  },
+  {
+    id: '3',
+    name: 'Stevan Bogosavljević',
+    employment: 'Software Development',
+    role: 'Mentor',
+    avatar: avatar3,
+  },
+  {
+    id: '4',
+    name: 'Filip Vučković',
+    employment: 'Software Development',
+    role: 'Mentor',
+    avatar: avatar4,
+  },
+  {
+    id: '5',
+    name: 'Ivan Radunović',
+    employment: 'Software Development',
+    role: 'Panelista',
+    avatar: avatar5,
+  },
+  {
+    id: '6',
+    name: 'Ognjen Bulajić',
+    employment: 'Graphic Design',
+    role: 'Mentor, Panelista',
+    avatar: avatar6,
+  },
+  {
+    id: '7',
+    name: 'Luka Žarić',
+    employment: 'Graphic Design',
+    role: 'Mentor',
+    avatar: avatar7,
+  },
+  {
+    id: '8',
+    name: 'Ivan Blažević',
+    employment: 'Customer Support',
+    role: 'Mentor, Panelista',
+    avatar: avatar8,
+  },
+  {
+    id: '9',
+    name: 'Marko Čurović',
+    employment: 'Content Writting',
+    role: 'Mentor',
+    avatar: avatar9,
+  },
+  {
+    id: '10',
+    name: 'Tijana Ostojić',
+    employment: 'Content Writting',
+    role: 'Mentor',
+    avatar: avatar10,
+  },
+  {
+    id: '11',
+    name: 'Nevena Korać',
+    employment: 'Digital Marketing',
+    role: 'Mentor, Panelista',
+    avatar: avatar11,
+  },
+  {
+    id: '12',
+    name: 'Srđan Borović ',
+    employment: 'Digital Marketing',
+    role: 'Mentor, Panelista',
+    avatar: avatar12,
+  },
+  {
+    id: '13',
+    name: 'Jovan Radnić',
+    employment: 'Digital Marketing',
+    role: 'Mentor',
+    avatar: avatar13,
+  },
+  {
+    id: '14',
+    name: 'Tjaša Deu',
+    employment: 'Youtube',
+    role: 'Panelista',
+    avatar: avatar14,
+  },
+];
+
+export default mentors;
