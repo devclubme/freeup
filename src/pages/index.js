e6e85edd24890473207c4efe3c11c22c42866dd3
goranc <cabarkapa.goran@gmail.com>
2021-05-25 12:20:09 +0200

    Updating title and title metatag

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,14 +16,14 @@ const App = () => {
   return (
     <>
       <Head>
-        <title>FreeUP</title>
+        <title>FreeUP | Tvoj put ka frilensu</title>
         <meta
           name="description"
           content="FreeUP je informativna i praktična obuka za sve mlade na početku karijere koji žele da se oprobaju kao frilenseri na globalnom tržištu."
         />
         {/* TODO: use actual url */}
         <meta property="og:url" content="https://www.freeup.me" />
-        <meta property="og:title" content="FreeUP" />
+        <meta property="og:title" content="FreeUP | Tvoj put ka frilensu" />
         <meta
           property="og:description"
           content="FreeUP je informativna i praktična obuka za sve mlade na početku karijere koji žele da se oprobaju kao frilenseri na globalnom tržištu."
